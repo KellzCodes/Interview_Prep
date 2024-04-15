290a1028844a23fa55733a11bfb822718f307574
Kelli <kellidavis320@gmail.com>
2024-04-15 17:59:04 -0400

    Reads a character string from the user and prints it.

---
+++ src/main/java/basics/chapter002/Echo.java
@@ -0,0 +1,22 @@
+package basics.chapter002;
+
+import java.util.Scanner;
+
+//********************************************************************
+// Echo.java Author: Lewis/Loftus
+//
+// Demonstrates the use of the nextLine method of the Scanner class
+// to read a string from the user.
+//********************************************************************
+public class Echo {
+    //-----------------------------------------------------------------
+    // Reads a character string from the user and prints it.
+    //-----------------------------------------------------------------
+    public static void main(String[] args){
+        String message;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a line of text:");
+        message = scan.nextLine();
+        System.out.println("You entered: \"" + message + "\"");
+    }
+}
